a5d70e34f805bcdff9fbda42f772bc485223c00c
Marlon Leon <ghostveene@gmail.com>
2023-02-02 23:56:44 -0500

    added eventlisteners for like and retweet

--- index.js
+++ index.js
@@ -5,7 +5,42 @@ const tweetInput = document.getElementById("tweet-input")
 
 tweetBtn.addEventListener("click", () => console.log(tweetInput.value))
 
+document.addEventListener("click", event => {
+    if (event.target.dataset.like) {
+        handleLikeClick(event.target.dataset.like)
+    } if (event.target.dataset.retweet) {
+        handleRetweetClick(event.target.dataset.retweet)
+    }
+})
 
+const handleLikeClick = (tweetId) => {
+    const targetTweetObj = tweetsData.filter(tweet => {
+        return tweet.uuid === tweetId
+    })[0]
+
+    if (targetTweetObj.isLiked) {
+        targetTweetObj.likes--
+        
+    } else {
+        targetTweetObj.likes++ 
+    }
+    targetTweetObj.isLiked = !targetTweetObj.isLiked
+    render()
+}
+
+const handleRetweetClick = (tweedId) => {
+    const targetTweetObj = tweetsData.filter(tweet => {
+        return tweet.uuid === tweedId
+    })[0]
+    
+    if (targetTweetObj.isRetweeted) {
+        targetTweetObj.retweets--
+    } else {
+        targetTweetObj.retweets++
+    }
+    targetTweetObj.isRetweeted = !targetTweetObj.isRetweeted
+    render()
+}
 
 const getFeedHtml = () => {
     let feedHtml = ""
