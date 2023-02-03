915eba2ef8cc6cbed70db6a6629ffc56e3c425c6
Marlon Leon <ghostveene@gmail.com>
2023-02-02 22:09:57 -0500

    gettingFeedHtml with for of

--- index.js
+++ index.js
@@ -2,5 +2,35 @@ import { tweetsData } from "./data.js"
 
 const tweetBtn = document.getElementById("tweet-btn")
 const tweetInput = document.getElementById("tweet-input")
+const feed = document.getElementById("feed")
 
 tweetBtn.addEventListener("click", () => console.log(tweetInput.value))
+
+const getFeedHtml = () => {
+    let feedHtml = ""
+    
+    for (let tweets of tweetsData) {
+        feedHtml += `<div class="tweet">
+                        <div class="tweet-inner">
+                            <img src="${tweets.profilePic}" class="profile-pic">
+                            <div>
+                                <p class="handle">${tweets.handle}</p>
+                                <p class="tweet-text">${tweets.tweetText}</p>
+                                <div class="tweet-details">
+                                    <span class="tweet-detail">
+                                        ${tweets.replies.length}
+                                    </span>
+                                    <span class="tweet-detail">
+                                        ${tweets.likes}
+                                    </span>
+                                    <span class="tweet-detail">
+                                        ${tweets.retweets}
+                                    </span>
+                                </div>   
+                            </div>            
+                        </div>
+                    </div>`
+    }
+}
+
+getFeedHtml()
